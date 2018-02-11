b53b0d0fca1897f615e06e55e9c9b25d2a6eb399
pach092 <pachobernal5@gmail.com>
2018-02-11 12:29:08 -0500

    Manipulacion del dom

--- platzi-music/src/App.vue
+++ platzi-music/src/App.vue
@@ -1,20 +1,53 @@
 <template lang="pug">
   #app
-    img(src="./assets/logo.png")
-    h1 {{ msg }}
+    section.section
+      nav.nav.has-shadow
+        .container
+          input.input.is-large(
+            type="text", 
+            placeholder="Buscar canciones", 
+            v-model="searchQuery")
+          a.button.is-info.is-large(@click="search") Buscar
+          a.button.is-danger.is-large &times;
+          p
+            small {{ searchMessage }}
+      .container.results
+        .columns
+          .column(v-for="t in tracks") {{ t.name }} - {{ t.artist }}
 </template>
 
 <script>
+const tracks = [
+  { name: 'Muchacha', artist: 'Luis Alberto Spinneta' },
+  { name: 'Hoy aca en el baile', artist: 'El Pepo' },
+  { name: 'I was made for loving you', artist: 'Kiss' }
+]
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Hola Vue!'
+      searchQuery: '',
+      tracks: []
+    }
+  },
+  computed: {
+    searchMessage () {
+      return `Encontrados ${this.tracks.length}`
+    }
+  },
+  methods: {
+    search () {
+      this.tracks = tracks
     }
   }
 }
 </script>
 
 <style lang="scss">
-@import './scss/main.scss'
+  @import './scss/main.scss';
+
+  .results {
+    margin-top: 50px
+  }
 </style>
